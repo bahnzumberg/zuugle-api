75e80556ba79f90ac27f199f677c63007d94d971
Manan Pujara <104253184+MananPoojara@users.noreply.github.com>
2024-04-21 18:52:35 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         {
             name: 'zuugle_api',
             script: './api/index.js',
-            log_date_format: "YYYY-MM-DD",
+            log_date_format: "YYYY-MM-DD HH:mm:ss",
             exec_mode: "fork_mode",
             env: {
                 NODE_ENV: 'production'
